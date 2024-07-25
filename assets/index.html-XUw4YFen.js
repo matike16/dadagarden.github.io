f3529420b9e6c9a5737b129d23ff4fdd5e5f0be5
matike16 <131516727+matike16@users.noreply.github.com>
2024-07-25 10:25:24 +0000

    Deploying to gh-pages from @ matike16/dadagarden.github.io@7a2b7040f6f2336cbe4bfa68bb63f9325c6ec78d 🚀

---
+++ assets/index.html-XUw4YFen.js
@@ -0,0 +1,98 @@
+import{_ as n,o as t,c as e,f as o}from"./app-DPoJ9CHN.js";const c={};function i(a,r){return t(),e("div",null,[o(` ---
+# 当前页面内容标题
+title: 导航
+# 当前页面图标
+icon: link
+date: 2024-07-24
+article: false
+breadcrumb: false
+pageInfo: false
+editLink: false
+contributors: false
+lastUpdated: false
+prev: false
+next: false
+# comment: false
+---
+
+## <HopeIcon icon="magnifying-glass" color="var(--theme-color)"/> 搜索引擎
+
+### 常规搜索
+
+<NavCard arr='[
+    {"title": "必应", "url": "https://cn.bing.com/", "desc": "优秀的国内外搜索引擎", "icon": "/navicon/bing.ico"},
+    {"title": "谷歌", "url": "https://www.google.com/", "desc": "被墙的国内外搜索引擎", "icon": "/navicon/google.ico"}
+]'></NavCard>
+
+### AI搜索
+
+<NavCard arr='[
+    {"title": "ChatGPT", "url": "https://chatgpt.com/", "desc": "国外知名AI大模型", "icon": "/navicon/chatgpt.png"},
+    {"title": "Kimi", "url": "https://kimi.moonshot.cn/", "desc": "优秀的国内AI大模型", "icon": "/navicon/kimi.png"}
+]'></NavCard>
+
+## <HopeIcon icon="code" color="var(--theme-color)"/> 技术相关
+
+### 开源社区
+
+<NavCard arr='[
+    {"title": "GitHub", "url": "https://github.com/", "desc": "全球最大的开源社区", "icon": "/navicon/github.ico"},
+    {"title": "Gitee", "url": "https://gitee.com/", "desc": "国内最大的开源社区", "icon": "https://gitee.com/static/images/logo-en.svg"}
+]'></NavCard>
+
+### 编程教程
+
+<NavCard arr='[
+    {"title": "IDEA", "url": "https://idea.javaguide.cn/", "desc": "IDEA高效使用指南", "icon": "/navicon/idea.svg"},
+    {"title": "QuickRef", "url": "https://quickref.cn/", "desc": "技术栈快速参考备忘清单", "icon": "/navicon/quickref.png"},
+    {"title": "C语言", "url": "http://c.biancheng.net/", "desc": "C语言中文网", "icon": "/navicon/c.ico"},
+    {"title": "菜鸟教程", "url": "https://www.runoob.com/", "desc": "菜鸟在线教程", "icon": "/navicon/runoob.ico"},
+    {"title": "W3C", "url": "https://www.w3cschool.cn/tutorial", "desc": "w3c在线基础教程", "icon": "/navicon/w3c.ico"},
+    {"title": "w3school", "url": "https://www.w3school.com.cn/index.html", "desc": "领先的web技术教程", "icon": "/navicon/w3school.png"}
+]'></NavCard>
+
+### 在线工具
+
+<NavCard arr='[
+    {"title": "uiverse", "url": "https://uiverse.io/", "desc": "一个开源免费的UI组件库", "icon": "/navicon/th.png"},
+    {"title": "vercel", "url": "https://vercel.com/", "desc": "一个前端部署平台", "icon": "/navicon/vercel.png"},
+    {"title": "iconfont", "url": "https://www.iconfont.cn/", "desc": "一个丰富的矢量图标库", "icon": "/navicon/iconfont.png"},
+    {"title": "fontawesome", "url": "https://fontawesome.com/", "desc": "一个国外的矢量图标库", "icon": "/navicon/fontawesome.png"}
+]'></NavCard>
+
+### 技术框架
+
+<NavCard arr='[
+    {"title": "Vuepress", "url": "https://vuepress.vuejs.org/zh/", "desc": "一个基于vue的静态网站生成器", "icon": "/navicon/vuepress.png"},
+    {"title": "ElementPlus", "url": "https://element-plus.org/zh-CN/", "desc": "一套基于 Vue 3.0 的桌面端组件库", "icon": "/navicon/element-plus.png"}
+]'></NavCard>
+
+### 技术博客
+
+<NavCard arr='[
+    {"title": "hope", "url": "https://theme-hope.vuejs.press/zh/", "desc": "基于vuepress的静态博客搭建工具", "icon": "/navicon/hope.svg"},
+    {"title": "编程宝典", "url": "https://www.codefather.cn/", "desc": "B站鱼皮的编程宝典", "icon": "/navicon/编程宝典.png"}
+]'></NavCard>
+
+### 代码刷题
+
+<NavCard arr='[
+    {"title": "LeetCode", "url": "https://leetcode.cn/", "desc": "一个在线编程刷题平台", "icon": "/navicon/leetcode.ico"},
+    {"title": "代码随想录", "url": "https://programmercarl.com/", "desc": "高效学习算法的网站", "icon": "/navicon/代码随想录.png"}
+]'></NavCard>
+
+## <HopeIcon icon="heart" color="var(--theme-color)"/> 个人兴趣
+
+### 影视网站
+
+<NavCard arr='[
+    {"title": "B站", "url": "https://www.bilibili.com/", "desc": "国内知名弹幕网站", "icon": "/navicon/bilibili.png"},
+    {"title": "HDmoli", "url": "https://www.hdmoli.pro/", "desc": "海外影视在线观看平台", "icon": "/navicon/HDmoli.png"},
+    {"title": "BTNULL", "url": "https://www.btnull.net/", "desc": "一个丰富的影视资源下载站", "icon": "/navicon/btnull.png"}
+]'></NavCard>
+
+### 游戏资源
+
+<NavCard arr='[
+    {"title": "Byruthub", "url": "https://byruthub.org/", "desc": "俄区盗版游戏资源网站", "icon": "/navicon/byruthub.png"}
+]'></NavCard> `)])}const s=n(c,[["render",i],["__file","index.html.vue"]]),p=JSON.parse(`{"path":"/quick-nav/","title":"","lang":"zh-CN","frontmatter":{"head":[["meta",{"property":"og:url","content":"https://matike16.github.io/dadagarden.github.io/dadagarden.github.io/quick-nav/"}],["meta",{"property":"og:site_name","content":"哒哒的花园"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2024-07-25T10:24:31.000Z"}],["meta",{"property":"article:author","content":"哒哒溜"}],["meta",{"property":"article:modified_time","content":"2024-07-25T10:24:31.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2024-07-25T10:24:31.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"哒哒溜\\",\\"url\\":\\"https://github.com/matike16\\"}]}"]]},"headers":[],"git":{"createdTime":1721813818000,"updatedTime":1721903071000,"contributors":[{"name":"matike16","email":"lda240820@gmail.com","commits":8}]},"readingTime":{"minutes":2.11,"words":632},"filePathRelative":"quick-nav/README.md","localizedDate":"2024年7月24日","excerpt":"<!-- ---\\n# 当前页面内容标题\\ntitle: 导航\\n# 当前页面图标\\nicon: link\\ndate: 2024-07-24\\narticle: false\\nbreadcrumb: false\\npageInfo: false\\neditLink: false\\ncontributors: false\\nlastUpdated: false\\nprev: false\\nnext: false\\n# comment: false\\n---\\n\\n## <HopeIcon icon=\\"magnifying-glass\\" color=\\"var(--theme-color)\\"/> 搜索引擎\\n\\n### 常规搜索\\n\\n<NavCard arr='[\\n    {\\"title\\": \\"必应\\", \\"url\\": \\"https://cn.bing.com/\\", \\"desc\\": \\"优秀的国内外搜索引擎\\", \\"icon\\": \\"/navicon/bing.ico\\"},\\n    {\\"title\\": \\"谷歌\\", \\"url\\": \\"https://www.google.com/\\", \\"desc\\": \\"被墙的国内外搜索引擎\\", \\"icon\\": \\"/navicon/google.ico\\"}\\n]'></NavCard>\\n\\n### AI搜索\\n\\n<NavCard arr='[\\n    {\\"title\\": \\"ChatGPT\\", \\"url\\": \\"https://chatgpt.com/\\", \\"desc\\": \\"国外知名AI大模型\\", \\"icon\\": \\"/navicon/chatgpt.png\\"},\\n    {\\"title\\": \\"Kimi\\", \\"url\\": \\"https://kimi.moonshot.cn/\\", \\"desc\\": \\"优秀的国内AI大模型\\", \\"icon\\": \\"/navicon/kimi.png\\"}\\n]'></NavCard>\\n\\n## <HopeIcon icon=\\"code\\" color=\\"var(--theme-color)\\"/> 技术相关\\n\\n### 开源社区\\n\\n<NavCard arr='[\\n    {\\"title\\": \\"GitHub\\", \\"url\\": \\"https://github.com/\\", \\"desc\\": \\"全球最大的开源社区\\", \\"icon\\": \\"/navicon/github.ico\\"},\\n    {\\"title\\": \\"Gitee\\", \\"url\\": \\"https://gitee.com/\\", \\"desc\\": \\"国内最大的开源社区\\", \\"icon\\": \\"https://gitee.com/static/images/logo-en.svg\\"}\\n]'></NavCard>\\n\\n### 编程教程\\n\\n<NavCard arr='[\\n    {\\"title\\": \\"IDEA\\", \\"url\\": \\"https://idea.javaguide.cn/\\", \\"desc\\": \\"IDEA高效使用指南\\", \\"icon\\": \\"/navicon/idea.svg\\"},\\n    {\\"title\\": \\"QuickRef\\", \\"url\\": \\"https://quickref.cn/\\", \\"desc\\": \\"技术栈快速参考备忘清单\\", \\"icon\\": \\"/navicon/quickref.png\\"},\\n    {\\"title\\": \\"C语言\\", \\"url\\": \\"http://c.biancheng.net/\\", \\"desc\\": \\"C语言中文网\\", \\"icon\\": \\"/navicon/c.ico\\"},\\n    {\\"title\\": \\"菜鸟教程\\", \\"url\\": \\"https://www.runoob.com/\\", \\"desc\\": \\"菜鸟在线教程\\", \\"icon\\": \\"/navicon/runoob.ico\\"},\\n    {\\"title\\": \\"W3C\\", \\"url\\": \\"https://www.w3cschool.cn/tutorial\\", \\"desc\\": \\"w3c在线基础教程\\", \\"icon\\": \\"/navicon/w3c.ico\\"},\\n    {\\"title\\": \\"w3school\\", \\"url\\": \\"https://www.w3school.com.cn/index.html\\", \\"desc\\": \\"领先的web技术教程\\", \\"icon\\": \\"/navicon/w3school.png\\"}\\n]'></NavCard>\\n\\n### 在线工具\\n\\n<NavCard arr='[\\n    {\\"title\\": \\"uiverse\\", \\"url\\": \\"https://uiverse.io/\\", \\"desc\\": \\"一个开源免费的UI组件库\\", \\"icon\\": \\"/navicon/th.png\\"},\\n    {\\"title\\": \\"vercel\\", \\"url\\": \\"https://vercel.com/\\", \\"desc\\": \\"一个前端部署平台\\", \\"icon\\": \\"/navicon/vercel.png\\"},\\n    {\\"title\\": \\"iconfont\\", \\"url\\": \\"https://www.iconfont.cn/\\", \\"desc\\": \\"一个丰富的矢量图标库\\", \\"icon\\": \\"/navicon/iconfont.png\\"},\\n    {\\"title\\": \\"fontawesome\\", \\"url\\": \\"https://fontawesome.com/\\", \\"desc\\": \\"一个国外的矢量图标库\\", \\"icon\\": \\"/navicon/fontawesome.png\\"}\\n]'></NavCard>\\n\\n### 技术框架\\n\\n<NavCard arr='[\\n    {\\"title\\": \\"Vuepress\\", \\"url\\": \\"https://vuepress.vuejs.org/zh/\\", \\"desc\\": \\"一个基于vue的静态网站生成器\\", \\"icon\\": \\"/navicon/vuepress.png\\"},\\n    {\\"title\\": \\"ElementPlus\\", \\"url\\": \\"https://element-plus.org/zh-CN/\\", \\"desc\\": \\"一套基于 Vue 3.0 的桌面端组件库\\", \\"icon\\": \\"/navicon/element-plus.png\\"}\\n]'></NavCard>\\n\\n### 技术博客\\n\\n<NavCard arr='[\\n    {\\"title\\": \\"hope\\", \\"url\\": \\"https://theme-hope.vuejs.press/zh/\\", \\"desc\\": \\"基于vuepress的静态博客搭建工具\\", \\"icon\\": \\"/navicon/hope.svg\\"},\\n    {\\"title\\": \\"编程宝典\\", \\"url\\": \\"https://www.codefather.cn/\\", \\"desc\\": \\"B站鱼皮的编程宝典\\", \\"icon\\": \\"/navicon/编程宝典.png\\"}\\n]'></NavCard>\\n\\n### 代码刷题\\n\\n<NavCard arr='[\\n    {\\"title\\": \\"LeetCode\\", \\"url\\": \\"https://leetcode.cn/\\", \\"desc\\": \\"一个在线编程刷题平台\\", \\"icon\\": \\"/navicon/leetcode.ico\\"},\\n    {\\"title\\": \\"代码随想录\\", \\"url\\": \\"https://programmercarl.com/\\", \\"desc\\": \\"高效学习算法的网站\\", \\"icon\\": \\"/navicon/代码随想录.png\\"}\\n]'></NavCard>\\n\\n## <HopeIcon icon=\\"heart\\" color=\\"var(--theme-color)\\"/> 个人兴趣\\n\\n### 影视网站\\n\\n<NavCard arr='[\\n    {\\"title\\": \\"B站\\", \\"url\\": \\"https://www.bilibili.com/\\", \\"desc\\": \\"国内知名弹幕网站\\", \\"icon\\": \\"/navicon/bilibili.png\\"},\\n    {\\"title\\": \\"HDmoli\\", \\"url\\": \\"https://www.hdmoli.pro/\\", \\"desc\\": \\"海外影视在线观看平台\\", \\"icon\\": \\"/navicon/HDmoli.png\\"},\\n    {\\"title\\": \\"BTNULL\\", \\"url\\": \\"https://www.btnull.net/\\", \\"desc\\": \\"一个丰富的影视资源下载站\\", \\"icon\\": \\"/navicon/btnull.png\\"}\\n]'></NavCard>\\n\\n### 游戏资源\\n\\n<NavCard arr='[\\n    {\\"title\\": \\"Byruthub\\", \\"url\\": \\"https://byruthub.org/\\", \\"desc\\": \\"俄区盗版游戏资源网站\\", \\"icon\\": \\"/navicon/byruthub.png\\"}\\n]'></NavCard> -->"}`);export{s as comp,p as data};
