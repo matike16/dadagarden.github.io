75075e0d690c56b5dd10d1c028b43fbe3fa0395c
matike16 <131516727+matike16@users.noreply.github.com>
2024-08-01 12:06:39 +0000

    Deploying to gh-pages from @ matike16/dadagarden.github.io@bbad3ccdd4256db5b86a22f2c02c8b6adf7a8786 🚀

---
+++ JS/Baidutongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?8ad50d6377be89a58f1d854dabcf1562";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
